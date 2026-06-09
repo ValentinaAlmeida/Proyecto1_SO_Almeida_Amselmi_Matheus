2ee22025b9cfbc4b3f9ddae8746c4ac437ec99f6
aleXxaAms <alexandraamselmi@gmail.com>
2026-06-08 20:59:47 -0400

    flechas listas

--- ucvsh/src/flechas.c
+++ ucvsh/src/flechas.c
@@ -5,6 +5,8 @@
 #include "../include/modos_shell.h"
 #include "../include/historial_comandos.h"
 
+#define BARRERA 7
+
 void habilitar_no_canonico(struct termios* modo){
     struct termios modo2;
     //https://www.ibm.com/docs/es/aix/7.3.0?topic=files-termiosh-file
@@ -38,7 +40,7 @@ int leer_actual(char* leido, size_t tamano){
     int caracter_ingresado;
     size_t posicion = 0;//posicion actual en la terminal
     int i = total_historial; //inicio el recorrido desde el fondo para que sepa hacia donde va a subir
-
+    int tam_actual=0;
     memset(leido, 0, tamano);//inicializo todo en 0 https://www.ibm.com/docs/es/i/7.5.0?topic=functions-memset-set-bytes-value
 
     while(1){
@@ -57,11 +59,37 @@ int leer_actual(char* leido, size_t tamano){
             break;
         }
         if (caracter_ingresado == '\b' || caracter_ingresado  == 127) {//para borrar cosas
+            if (posicion <= 0) {
+                    continue;
+                }
             if (posicion > 0) {
+                
+                if (tam_actual==posicion) {
+                
                 posicion--;
+                tam_actual--;
                 leido[posicion] = '\0';
+                
                 printf("\b \b"); 
                 fflush(stdout);
+                }else{
+                    
+                    printf("\b");
+                    posicion--;
+                    
+                    for (size_t a = posicion; a < (size_t)tam_actual; a++) {
+                        leido[a] = leido[a + 1];//ruedo todo uno a la izquierda desde la posicion que borre
+                    }
+                    tam_actual--;
+                    
+                    leido[tam_actual] = '\0';
+                    printf("%s\033[K", &leido[posicion]);//vuelvo a imprimir para que se pueda ver
+                
+                    for (int h2 = tam_actual; h2 > (int)posicion; h2--) {//como el cursor se movio debo devolverlo
+                    printf("\b");//moverme a la izquierda
+                }
+                    fflush(stdout);//me aseguro de que todo imprima
+                }
             }
             continue;//saltate todo y continua
         }
@@ -87,6 +115,7 @@ int leer_actual(char* leido, size_t tamano){
                         strncpy(leido, arreglo_memoria[i], tamano);
                         leido[tamano-1]='\0';
                         posicion = strlen(leido);
+                        tam_actual=posicion;
                         printf("%s", leido);
                         fflush(stdout);
                     }
@@ -108,16 +137,30 @@ int leer_actual(char* leido, size_t tamano){
                             leido[tamano-1] = '\0';
                         }
                         posicion = strlen(leido);
+                        tam_actual=posicion;
                         printf("%s", leido);
                         fflush(stdout);
                     }
                     continue;
                     break;
                     case 'C'://derecha
-
+                    if (tam_actual > posicion) {
+                            printf("\033[C");//la instruccion para que se mueva uno a la derecha pero sin sobreescribir nada
+                            //ANSI SCAPE CODE en wikipedia de las referencias bibliograficas lo explica :)
+                            posicion++;   // aumento porque me movi
+                            
+                            fflush(stdout);
+                        }
                     break;
                     case 'D'://izquierda
+                        if (posicion > 0) {
+                            printf("\b"); // Me muevo visualmente a la izquierda, ya abajo trabajo inserciones
 
+                            posicion--;   // decremento la posicion ya que me movi
+                            
+                            fflush(stdout);
+                        }
+                        continue;
                     break;
                 }
             }
@@ -125,11 +168,30 @@ int leer_actual(char* leido, size_t tamano){
             
     }
     if (posicion < (tamano - 1)) {
-            
+            if (posicion == tam_actual) {//si esta al final simplemente escribe
             leido[posicion] = caracter_ingresado;
             posicion++;
+            tam_actual++;
+            leido[tam_actual] = '\0';
             putchar(caracter_ingresado); // imprimo el caracter porque como echo esta apagado no imprime solo
             fflush(stdout);//asegurate de que todo se imprima antes de continuar
+            }else{
+
+                for(int g=tam_actual; (int)posicion < g; g--){
+                    leido[g]=leido[g-1];//para evitar cortar cadenas y concatenaciones, ruedo todo un espacio hacia la derecha y ya antes vi si habia espacio
+                }
+                tam_actual++;//incremento tamaño
+                leido[posicion]= caracter_ingresado;//añado el caracter donde abri espacio
+                posicion++;//cambio de posicion
+                leido[tam_actual]='\0';//añado el null
+                printf("%s", &leido[posicion - 1]);//vuelvo a imprimir para que se pueda ver
+                
+                for(int h=posicion; h < tam_actual; h++){//como volvi a imprimir el cursor me quedaba al final, asi que debia devolverlo
+                    printf("\b");//me muevo a la izquierda
+                }
+                fflush(stdout);//me aseguro de que todo imprima
+            }
+            
         }
     }
 
