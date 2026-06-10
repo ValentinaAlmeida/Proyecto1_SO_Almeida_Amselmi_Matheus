6453b198db2983095741113bd84cc48a186b42d2
MagiaGatuna <corinavicky07@gmail.com>
2026-06-10 18:03:08 -0400

    ctrl Z resuelto y doble impresion de ucvsh en ctrl c resuelto by Valentina y Corina

--- ucvsh/src/signals.c
+++ ucvsh/src/signals.c
@@ -20,41 +20,16 @@ void capturar_senal(int senal){
         if (pid_primer_plano > 0) {
             kill(pid_primer_plano, SIGINT);
         } else {
-            struct termios modo;//lo mismo del codigo del no canonico de flechas.h para reactivar o pasaban cosas raras cuando probamos
-            tcgetattr(STDIN_FILENO, &modo);
-            modo.c_lflag |= (ICANON | ECHO);
-            tcsetattr(STDIN_FILENO, TCSAFLUSH, &modo);
-
-            // 2. Imprimimos el prompt exactamente igual al del main (Morado y protegido)
-            printf("\n" MORADO "ucvsh" LETRA_NORMAL MORADO "> " LETRA_NORMAL);
+            
+            printf("\n");
             fflush(stdout);
         }
     } 
     else if (senal == SIGTSTP){ // Ctrl+Z
         if (pid_primer_plano > 0) {
             kill(pid_primer_plano, SIGTSTP); // Pausa el proceso hijo
-            
-            // Buscamos el proceso en la lista para cambiar su estado al número 2
-            Job* trabajo = Buscar_job_porpid(pid_primer_plano, lista_jobs);
-            if (trabajo != NULL) {
-                trabajo->estado = 2; 
-            } else {
-                Insertar_job(&lista_jobs, pid_primer_plano, comando_primer_plano, 2);
-            }
-            struct termios modo;
-            tcgetattr(STDIN_FILENO, &modo);
-            modo.c_lflag |= (ICANON | ECHO);
-            tcsetattr(STDIN_FILENO, TCSAFLUSH, &modo);
-
-            printf("\n[Detenido] Usa 'jobs' o 'fg' para gestionarlo\n" MORADO "ucvsh" LETRA_NORMAL MORADO "> " LETRA_NORMAL);
-            fflush(stdout);
-        } else {
-            struct termios modo;
-            tcgetattr(STDIN_FILENO, &modo);
-            modo.c_lflag |= (ICANON | ECHO);
-            tcsetattr(STDIN_FILENO, TCSAFLUSH, &modo);
-
-            printf("\n" MORADO "ucvsh" LETRA_NORMAL MORADO "> " LETRA_NORMAL);
+        } else{
+            printf("\n");
             fflush(stdout);
         }
     }
