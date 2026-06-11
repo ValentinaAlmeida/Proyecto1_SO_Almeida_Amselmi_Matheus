0c8a4aa44f71c2c82e5d6586302e189263ada044
ValentinaAlmeida <v.antoima2002@gmail.com>
2026-06-11 16:22:10 -0400

    Agregando comentarios a signals.c

--- ucvsh/src/signals.c
+++ ucvsh/src/signals.c
@@ -1,4 +1,4 @@
-//# {} [] > < || &&
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
@@ -8,44 +8,40 @@
 #include "../include/signals.h"
 #include "../include/job_list.h"
 #include "../include/colores.h" 
+volatile pid_t pid_primer_plano = 0;//aqui es donde definimos la verdadera varible que guarada el pid de el proceso que esta ctaulemnte en primer plano
+char comando_primer_plano[1024] = "";//aqui guardaremos el comando que esta en primer plano 
 
-// VARIABLES GLOBALES DE SEÑALES
-volatile pid_t pid_primer_plano = 0;
-char comando_primer_plano[1024] = "";
+struct Job* lista_jobs; //aqui se crea la lista de jobs 
 
-struct Job* lista_jobs; 
+void capturar_senal(int senal){//como vimos en clses, hacemso el manejador de senales, determiian que se hace si se recibe una senal
+    if(senal == SIGINT){ // Ctrl+C, si sla senal es  esa 
+        if (pid_primer_plano > 0) {//si el pid de el proceso es mayor a cero( es un proceos que esta creado)
+            kill(pid_primer_plano, SIGINT);//le enviamos una senal con la llamada al sistema de qu ahora debe terinar 
+        } else {//en caso contrrio el proceso no esta
+            printf("\n");//no se imprime nada 
+            fflush(stdout);//se vacia el buffer 
+        }//cierre de la verificaciones
+    } //cierre del if
+    else if (senal == SIGTSTP){ // Ctrl+Z, si la senal resulta ser esta
+        if (pid_primer_plano > 0) {//si el proceso esta creado
+            kill(pid_primer_plano, SIGTSTP); // Pausa el proceso hijo, se el envia la senal a traves de el kill
+        } else{//si no esta creado
+            printf("\n");//no imprimos nada 
+            fflush(stdout);//vacioamos el buffer
+        }//cieere de verificacones
+    }//cieere de verificaciones
+}//fin de la funcion 
 
-void capturar_senal(int senal){
-    if(senal == SIGINT){ // Ctrl+C
-        if (pid_primer_plano > 0) {
-            kill(pid_primer_plano, SIGINT);
-        } else {
-            
-            printf("\n");
-            fflush(stdout);
-        }
-    } 
-    else if (senal == SIGTSTP){ // Ctrl+Z
-        if (pid_primer_plano > 0) {
-            kill(pid_primer_plano, SIGTSTP); // Pausa el proceso hijo
-        } else{
-            printf("\n");
-            fflush(stdout);
-        }
-    }
-}
+void Manejadores_senales(){//aquies donde usamos sigaction() como semenciono en clases y labs
+    struct sigaction sa;//definimos la estructura de la senal
+    sa.sa_handler = capturar_senal;//el manejador que tendra
+    sigemptyset(&sa.sa_mask);//cualquier otra senal la puedes atender cualquiero otra que no sea la definida por mi con el menejador  cuando se haga este manejador el kernel de el SO ya no devuelva errores y solo regresse alpunto donde se quedo , interrumpe abruptamente cualquier llamada al sistema que la shell estuviera haciendo en ese momento
+    sa.sa_flags = SA_RESTART; //se configura sus banderas, esta para que 
 
-void Manejadores_senales(){
-    struct sigaction sa;
-    sa.sa_handler = capturar_senal;
-    sigemptyset(&sa.sa_mask);
-    sa.sa_flags = SA_RESTART; 
-
-    if (sigaction(SIGINT, &sa, NULL) == -1) {
-        perror(ROJO LETRA_NEGRITA"Error al configurar SIGINT"LETRA_NORMAL);
-    }
-    if (sigaction(SIGTSTP, &sa, NULL) == -1) {
-        perror(ROJO LETRA_NEGRITA"Error al configurar SIGTSTP"LETRA_NORMAL);
-    }
-}
-//# {} [] > < || &&
+    if (sigaction(SIGINT, &sa, NULL) == -1) {//se define la senal a capturar para Ctrl+C si esta devuelve error
+        perror(ROJO LETRA_NEGRITA"Error al configurar SIGINT"LETRA_NORMAL);//se el notifica a el usuario
+    }//cierre del if
+    if (sigaction(SIGTSTP, &sa, NULL) == -1) {//se define la senal a capturar para Ctrl+Z si esta devuelve error
+        perror(ROJO LETRA_NEGRITA"Error al configurar SIGTSTP"LETRA_NORMAL);//se le notifica a el usuario
+    }//cierre de veirifcacones
+}//cieere de la funcion 
